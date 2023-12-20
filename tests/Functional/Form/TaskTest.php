d6af094ebf17e5dff508d1c2cf7a30b5016b0492
znguyen <znguyen@getraline.com>
2023-12-20 15:59:38 +0100

    Add test for TaskType form

---
+++ tests/Functional/Form/TaskTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Functional\Form;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TaskTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/tasks/create');
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertEquals(1, $crawler->filter('input[name="task[title]"]')->count());
+        $this->assertEquals(1, $crawler->filter('textarea[name="task[content]"]')->count());
+        $this->assertEquals(1, $crawler->filter('button')->count());
+    }
+}
